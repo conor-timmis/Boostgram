0b7673281f6789ac4bab4814794790db938c4d2a
Conor Timmis <55244147+conor-timmis@users.noreply.github.com>
2025-01-08 16:33:52 +0000

    Add Navbar to App

--- boostgram/src/App.tsx
+++ boostgram/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
 import LandingPage from './components/LandingPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App: React.FC = () => {
   return (
     <div className="App">
+      <Navbar />
       <LandingPage />
     </div>
   );
