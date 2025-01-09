b12c61f5afb42434de909c0f602c835ae9a5bb58
Conor Timmis <55244147+conor-timmis@users.noreply.github.com>
2025-01-09 16:06:50 +0000

    Add FontAwesome Icons to Navbar headings

--- boostgram/src/components/Navbar.tsx
+++ boostgram/src/components/Navbar.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import './Navbar.css'; // Import custom CSS for styling
+import '@fortawesome/fontawesome-free/css/all.min.css'; // Import FontAwesome CSS
 
 const Navbar: React.FC = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -13,7 +14,9 @@ const Navbar: React.FC = () => {
             <nav className="navbar">
                 {/* Logo or brand name */}
                 <div className="logo">
-                    <a href="/">Boostgram</a>
+                    <a href="/">
+                        <i className="fas fa-rocket"></i> Boostgram
+                    </a>
                 </div>
 
                 {/* Hamburger menu for mobile */}
@@ -25,10 +28,26 @@ const Navbar: React.FC = () => {
 
                 {/* Navigation Links */}
                 <ul className={`nav-links ${isMenuOpen ? 'active' : ''}`}>
-                    <li><a href="/#Login">Sign In</a></li>
-                    <li><a href="/#Register">Sign Up</a></li>
-                    <li><a href="/#pricing">Pricing</a></li>
-                    <li><a href="/terms">Terms</a></li>
+                    <li>
+                        <a href="/#Login">
+                            <i className="fas fa-sign-in-alt"></i> Sign In
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/#features">
+                            <i className="fas fa-lightbulb"></i> Features
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/#pricing">
+                            <i className="fas fa-dollar-sign"></i> Pricing
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/terms">
+                            <i className="fas fa-file-contract"></i> Terms
+                        </a>
+                    </li>
                 </ul>
             </nav>
         </header>
