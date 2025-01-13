fe8aa5af48fdae58afc1fa0e26bd8f6c30e5da12
Conor Timmis <55244147+conor-timmis@users.noreply.github.com>
2025-01-13 17:35:06 +0000

    Add Terms & Conditions component to App

--- boostgram/src/App.tsx
+++ boostgram/src/App.tsx
@@ -2,14 +2,20 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import LandingPage from './components/LandingPage';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import TermsAndConditions from './components/TermsAndConditions';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <Navbar />
-      <LandingPage />
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/terms" element={<TermsAndConditions />} />
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
