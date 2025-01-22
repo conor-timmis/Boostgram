69e406cb49e8ff481c3d840e1ab63a7dc19cd1e2
Conor Timmis <55244147+conor-timmis@users.noreply.github.com>
2025-01-22 17:50:00 +0000

    Default the axiosDefaults API connection

--- boostgram/src/api/axiosDefaults.ts
+++ boostgram/src/api/axiosDefaults.ts
@@ -1,9 +1,7 @@
 import axios from "axios";
 
-// Update baseURL to point to your local backend
-axios.defaults.baseURL = "http://localhost:5000";
-
-axios.defaults.headers.post["Content-Type"] = "application/json";
+axios.defaults.baseURL = "https://localhost:5000/";
+axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
 export const axiosReq = axios.create();
